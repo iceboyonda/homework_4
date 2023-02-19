8e12c7197acc34503d5962f1f146b4104bc23cae
iceboy.on.da <oy.shit.205@mail.ru>
2023-02-19 22:02:30 +0300

    1

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
- const student = "Зима Дмитрий Владимирович"; 
- // Очевидно, что здесь ваши личные Фамилия, Имя и Отчество
+const student = "Зима Дмитрий Владимирович"; 
 
 document.getElementById("student").innerHTML = student;
 
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
- return Math.floor(Math.random() * (max - min + 1)) + min;
- }
-  function throwRandomError(errorRate) {
- 
-   if (getRandomIntInclusive(0 , 100) <= errorRate) {
-      throw new Error('Делает мозг');
+function getRandomIntInclusive(min, max) { //Начало функции, выводящящей значение от 0 до 2
+ min = Math.ceil(min); //округление в меньшую сторону
+ max = Math.floor(max); //округление в большую сторону
+return Math.floor(Math.random() * (max - min + 1)) + min; // Расшифровка:Рандом число сгенерированное 6й строкой умножить на (максимум - минимум + 1) + минимум
 }
-}
-const errorRate = 0.2;
+ function throwRandomError(errorRate) { //Начало второй функции (Ошибка)
 
-try 
+  if (getRandomIntInclusive(0 , 100) <= errorRate) { //Если первая функция > второй, выводит новую ошибку 
+     throw new Error('Ой, что-то сломалось'); //Ошибка которая выводится при (строка 13)
+}
+}
+const errorRate = 0.2; // Константа вероятности ошибки (Заданна условиями)
+
+try //блок, который ловит наличие ошибки
 {
-  throwRandomError(errorRate) ;
-  
-  alert('Робит');
-  
-  } 
-  catch (err) 
-  {
- alert ('Не робит');
-  console.log(err);
-  }
+ throwRandomError(errorRate) ; //Ловит ошибку, при ее отсутствии выводит на сайт
+ 
+ alert('Работает!)'); 
+
+ console.log(not.eror); //Ловит ошибку, при ее отсутствии выводит в консоль
+ 
+ } 
+ catch (err) // Ловит ошибку, при ее наличии выводит на сайте
+ {
+alert ('Сломалось(');
+ console.log(eror); //Ловит ошибку, при ее наличии выводит в консоль
+ }
